f9e52cab47a62907318c3a5c8f1e793c42e22553
hcaohr <huarong10dz@gmail.com>
2021-02-01 17:29:49 +0800

    修改config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from pathlib import Path
 class CONST:
     try:
         PROJECT_PATH = Path(os.path.dirname(__file__))
-        DATAPATH = str(PROJECT_PATH / Path('data.yml'))
-        DatPATH = str(PROJECT_PATH / Path('test_in_theaters.yml'))
+
+        CSV_PATH = str(PROJECT_PATH / Path('data/csv/example.csv'))
 
     except Exception as msg:
         print(msg)
